1ab3d3471ff304321042fc70fae05c0f9369b29f
ValentinMezev <britain444@gmail.com>
2015-01-20 22:37:37 +0200

    First Git commit...

---
+++ TestGitProject/src/com/test/git/Gosho.java
@@ -0,0 +1,12 @@
+package com.test.git;
+
+public class Gosho {
+	private final int numberOfLegs = 3;
+	private final boolean hasBrain = false;
+	
+	public static void main (String args[]) {
+		
+	}
+	
+	
+}
